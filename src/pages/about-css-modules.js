cbb04deaded1a8b2f6e74907d832bc21ef6cd14f
Roberts Kalnins <rkalnins314@gmail.com>
2018-07-05 18:57:45 -0400

    CSS modules

---
+++ src/pages/about-css-modules.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Container from "../components/container";
+
+export default () => (
+  <Container>
+    <h1>About CSS Modules</h1>
+    <p>CSS modules are amazing</p>
+  </Container>
+);

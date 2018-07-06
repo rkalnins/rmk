49cb165624cdc861c0445d8165c66d5bf4b03849
Roberts Kalnins <rkalnins314@gmail.com>
2018-07-06 00:49:01 -0400

    Update content

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,18 +1,27 @@
 import React from "react";
 import Link from "gatsby-link";
 
-const ListLink = props =>
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+const ListLink = props => (
+  <li style={{ display: `inline-block`, marginRight: `1.0rem` }}>
     <Link to={props.to}>
       {props.children}
     </Link>
   </li>
+);
+
+const FooterLinks = props => (
+  <li>
+    <Link to={props.to}>
+      {props.children}
+    </Link>
+  </li>
+);
 
 export default ({ children }) => (
   <div style={{ margin: `0 auto`, maxWidth: 650, padding:  `1.25rem 1rem` }}>
     <header style={{ marginBottom: `1.5rem` }}>
       <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-        <h3 style={{ display: `inline` }}>My site</h3>
+        <h3 style={{ display: `inline` }}>Roberts Kalnins</h3>
       </Link>
       <ul style={{ listStyle: `none`, float: `right` }}>
         <ListLink to="/">Home</ListLink>
@@ -21,5 +30,13 @@ export default ({ children }) => (
       </ul>
     </header>
   {children()}
+  <footer style={{ margin: `0 auto`, maxWidth: 650, padding: `1.25rem 1rem` }}>
+    <br/>
+    <ul style={{ listStyle: `none`, float: 'left' }}>
+      <FooterLinks to="/">Home</FooterLinks>
+      <FooterLinks to="/about/">About</FooterLinks>
+      <FooterLinks to="/contact/">Contact</FooterLinks>
+    </ul>
+  </footer>
   </div>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,21 +3,11 @@ import Link from "gatsby-link";
 
 export default () => (
   <div style={{ margin: '3rem auto', maxWidth: 600 }}>
-    <h1>Roberts Kalnins</h1>
     <div>
       <p>
         I am Roberts Kalnins.
       </p>
-      <blockquote>
-        <p>
-          This is a website.
-          <em>
-            Testing.....
-          </em>
-        </p>
-      </blockquote>
     </div>
-    <p>Posted April 09, 2011</p>
     <div>
       <Link to="/newpage/">Link to another page</Link>
     </div>
